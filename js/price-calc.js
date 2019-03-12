2f46821642169be05889a8a84e2dec286c2ac1ab
Nyolczas <nyolczas@gmail.com>
2019-03-12 22:14:10 +0100

    format time

--- js/price-calc.js
+++ js/price-calc.js
@@ -1,19 +1,20 @@
 // duration slider
-
+var formaTime = new Date;
 var durSlider = document.getElementById("durationR");
 var durOutput = document.getElementById("demoDuration");
-durOutput.innerHTML = durSlider.value;
+durOutput.innerHTML = formaTime.clearTime().addSeconds(durSlider.value).toString('H:mm:ss');
 
 durSlider.oninput = function() {
-  durOutput.innerHTML = this.value;
+  durOutput.innerHTML = formaTime.clearTime().addSeconds(this.value).toString('H:mm:ss');
 }
 
 // quality slider
+
 
 var qualSlider = document.getElementById("qualityR");
 var qualOutput = document.getElementById("demoQuality");
 qualOutput.innerHTML = qualSlider.value;
 
-qualSlider.oninput = function() {
+qualSlider.oninput = function() { 
   qualOutput.innerHTML = this.value;
 }
